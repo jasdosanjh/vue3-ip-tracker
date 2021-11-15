921090f2ac1e7d33b5f7799b72fc62e0795ee7ca
Jas Dosanjh <jas@dosanjh.co.uk>
2021-11-15 08:32:30 +0000

    setup home route, and scaffolded header component with search input field

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
+import "./assets/tailwind.css";
 import App from "./App.vue";
+import Home from "./views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+      meta: {
+        title: "IP Address Tracker",
+      },
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 createApp(App).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <div
+      class="
+        flex
+        justify-center
+        realtive
+        bg-header-pattern bg-cover
+        px-4
+        pt-8
+        pb-32
+      "
+    >
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="
+              flex-1
+              py-3
+              px-2
+              rounded-tl-md rounded-bl-md
+              focus:outline-none
+            "
+            type="text"
+            placeholder="Please enter an IP address or leave empty for your IP information"
+          />
+          <i
+            class="
+              cursor-pointer
+              bg-black
+              text-white
+              px-4
+              rounded-tr-md rounded-br-md
+              flex
+              items-center
+              fas
+              fa-chevron-right
+            "
+          ></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  components: {},
+};
+</script>
